9f1d9db47a76bd8c291575be3d43e04ca7c50f01
coolrunner1 <153802775+coolrunner1@users.noreply.github.com>
2025-02-08 10:05:46 +0300

    Added random color distribution

--- src/components/Cell.tsx
+++ src/components/Cell.tsx
@@ -1,7 +1,44 @@
-export const Cell = () => {
+import {useEffect, useState} from "react";
+
+export const Cell = (props: {id: number}) => {
+    const [bgColor, setBgColor] = useState<string>();
+
+    const colorDistribution = () => {
+        switch (Math.floor(props.id/2)){
+            case 0:
+                setBgColor("bg-green-400");
+                break;
+            case 1:
+                setBgColor("bg-red-400");
+                break;
+            case 2:
+                setBgColor("bg-yellow-400");
+                break;
+            case 3:
+                setBgColor("bg-purple-400");
+                break;
+            case 4:
+                setBgColor("bg-amber-600");
+                break;
+            case 5:
+                setBgColor("bg-cyan-400");
+                break;
+            case 6:
+                setBgColor("bg-emerald-700");
+                break;
+            case 7:
+                setBgColor("bg-gray-700");
+                break;
+        }
+    }
+
+    useEffect(() => {
+
+    }, [props.id]);
+
     return (
         <>
-            <div className="bg-gray-300 p-4"></div>
+            <div onClick={colorDistribution} id={`cell-${props.id}`} className={`p-4 rounded-xl ${bgColor ? bgColor : "bg-gray-300"}`}></div>
         </>
     );
 };
--- src/components/MainContainer.tsx
+++ src/components/MainContainer.tsx
@@ -1,15 +1,40 @@
 import {Cell} from "./Cell.tsx";
+import {useEffect, useState} from "react";
 
 export const MainContainer = () => {
+    const [array, setArray] = useState<number[]>([]);
+    const [round, setRound] = useState(0);
+
+    const shuffle = () => {
+        const array = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15];
+
+        let currentIndex = array.length;
+
+        while (currentIndex != 0) {
+            const randomIndex = Math.floor(Math.random() * currentIndex);
+            currentIndex--;
+
+            [array[currentIndex], array[randomIndex]] = [
+                array[randomIndex], array[currentIndex]];
+        }
+
+        setRound(round+1)
+        return array;
+    }
+
+    useEffect(() => {
+        setArray(shuffle());
+        console.log(array);
+    }, [])
+
     return (
         <>
             <div className="h-screen w-screen bg-white dark:bg-gray-800 flex flex-col items-center justify-center">
-                <div className="margin-auto w-full md:max-w-[500px] xl:max-w-[700px] h-full md:h-[70%] p-10 bg-blue-500 text-center m-auto gap-3 text-white md:rounded-2xl shadow-2xl">
+                <div className="margin-auto w-full md:min-h-[400px] md:max-w-[500px] xl:max-w-[700px] h-full md:h-[70%] p-10 bg-blue-500 text-center m-auto gap-3 text-white md:rounded-2xl shadow-2xl">
                     <h1 className="text-2xl font-bold">Find a Couple</h1>
-                    <div className="text-xl font-light">Round 1</div>
+                    <div className="text-xl font-light">Round {round}</div>
                     <div className="grid grid-cols-4 grid-rows-5 gap-6 h-full m-10">
-                        {[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16].map((item) => (<Cell/>))}
-
+                        {array.map((item, key) => (<Cell key={key} id={item} />))}
                     </div>
                 </div>
             </div>
